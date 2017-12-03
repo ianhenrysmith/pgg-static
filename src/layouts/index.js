dddf9bb740f4ecdd7007f3e27d8d0ba13b40b17f
Ian Smith <ianhenrysmith@gmail.com>
2017-12-03 07:54:03 -0700

    added mailchimp subscribe form

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import PropTypes from "prop-types"
 import Link from "gatsby-link"
 import Helmet from "react-helmet"
+import SubscribeForm from "react-mailchimp-subscribe";
 
 import "./index.css"
 import "./style.css"
@@ -35,6 +36,35 @@ class TemplateWrapper extends React.Component {
     )
   }
 
+  renderSubscribeForm() {
+    const formProps = {
+      action: "https://gifts.us17.list-manage.com/subscribe/post?u=11ab56c29bb4a46b3d75765e9&amp;id=257208557b",
+      messages: {
+        inputPlaceholder: "email address",
+        btnLabel: "Subscribe",
+        sending: "Subscribing...",
+        success: "Thank you for subscribing!",
+        error: "Oops, couldn't subscribe..."
+      },
+      styles: {
+        sending: {
+          fontSize: 18,
+          color: "auto"
+        },
+        success: {
+          fontSize: 18,
+          color: "green"
+        },
+        error: {
+          fontSize: 18,
+          color: "red"
+        }
+      }
+    }
+
+    return <SubscribeForm {...formProps}/>
+  }
+
   render() {
     return (
       <div>
@@ -48,6 +78,9 @@ class TemplateWrapper extends React.Component {
         { this.renderHeader() }
         <div className="body-container">
           {this.props.children()}
+        </div>
+        <div className="footer-container">
+          {this.renderSubscribeForm()}
         </div>
       </div>
     )
