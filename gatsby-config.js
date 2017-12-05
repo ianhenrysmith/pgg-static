c9f76afab759e4836c7fdd90c91156e36c41e5eb
Ian Smith <ianhenrysmith@gmail.com>
2017-12-05 07:35:11 -0700

    added favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -16,6 +16,24 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-transformer-json`,
     `gatsby-plugin-react-helmet`,
-    `gatsby-plugin-offline`
+    `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-favicon`,
+      options: {
+        logo: "./src/favicon.png",
+        injectHTML: true,
+        icons: {
+          android: true,
+          appleIcon: true,
+          appleStartup: true,
+          coast: false,
+          favicons: true,
+          firefox: true,
+          twitter: false,
+          yandex: false,
+          windows: false
+        }
+      }
+    }
   ]
 }
